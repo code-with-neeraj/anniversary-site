1f69acce78dbfc29aa1b0d74c213b0234762335e
code-with-neeraj <neerajkr145518@gmail.com>
2025-05-26 15:20:48 +0530

    add date

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,7 +12,7 @@ import FloatingElements from "@/components/FloatingElements"
 import TapToReveal from "@/components/TapToReveal"
 
 // Change this to your anniversary date
-const ANNIVERSARY_DATE = "2025-05-24T00:00:00"
+const ANNIVERSARY_DATE = "2025-05-27T00:00:00"
 // Change this to the date you got together
 const TOGETHER_DATE = "2021-05-27T00:00:00"
 
